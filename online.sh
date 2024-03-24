72e2d8a12355c303460401f74a1b820efa3e08fd
AnuBitux <122746698+AnuBitux@users.noreply.github.com>
2024-03-24 15:49:22 +0100

    Update online.sh

--- online.sh
+++ online.sh
@@ -3,5 +3,4 @@
 sudo nmcli radio wifi on
 sudo nmcli networking on
 sudo systemctl start bluetooth.service
-gsettings set org.cinnamon.desktop.background picture-uri  "file:/home/anubitux/Pictures/AnubituxWallpaperBlue.png"
-
+gsettings set org.cinnamon.desktop.background picture-uri  "file:/usr/share/backgrounds/AnuBitux_WP/AnubituxWallpaperBlue.png"
