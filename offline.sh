17343fcad9ab650a5965089133e87cb4cf12addd
AnuBitux <122746698+AnuBitux@users.noreply.github.com>
2024-03-24 15:49:02 +0100

    Update offline.sh

--- offline.sh
+++ offline.sh
@@ -4,6 +4,5 @@ sudo nmcli radio wifi off
 sudo nmcli networking off
 sudo systemctl disable bluetooth.service
 sudo systemctl stop bluetooth.service
-gsettings set org.cinnamon.desktop.background picture-uri  "file:/home/anubitux/Pictures/AnubituxWallpaperBlueOffline.png"
-
+gsettings set org.cinnamon.desktop.background picture-uri  "file:/usr/share/backgrounds/AnuBitux_WP/AnubituxWallpaperBlueOffline.png"
 
